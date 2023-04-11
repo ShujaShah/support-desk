cd69494d0255cdd3b6f97985e72143b17a000c8b
ShujaShah <shuja.ashraf11@gmail.com>
2023-04-11 23:39:34 +0530

    completed the notes

--- front-end/src/app/store.js
+++ front-end/src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import ticketReducer from "../features/tickets/ticketSlice";
+import noteReducer from '../features/notes/noteSlice'
 
 //here we put all the global states to be changed throughout the app
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     tickets: ticketReducer,
+    notes: noteReducer,
   },
 });
