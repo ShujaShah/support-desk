98b325047342cfac5b3403c1731441412fea44f7
ShujaShah <shuja.ashraf11@gmail.com>
2023-04-12 00:29:29 +0530

    removed the get / as conflict

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello how are you" });
-});
+// app.get("/", (req, res) => {
+//   res.status(200).json({ message: "Hello how are you" });
+// });
 
 //Routes
 app.use("/api/users", require("./routes/userRoutes"));
