3a175ae1fc26ea2404ef53c985f4a06f5b2e4c92
ShujaShah <shuja.ashraf11@gmail.com>
2023-04-12 00:20:52 +0530

    added the deploy config

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const colors = require('colors');
+const path = require('path');
 
 const connectDB = require('./config/db')
 
@@ -23,4 +24,17 @@ app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
 app.use(errorHandler);
 
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../front-end/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(__dirname, '../', 'front-end', 'build', 'index.html')
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' })
+  })
+}
 app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`));
